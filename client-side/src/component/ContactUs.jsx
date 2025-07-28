367bb252c41ce92516daca2031760ac2f6cbed3d
Nikhil Tiwari <nikhilhuh@gmail.com>
2025-07-28 20:44:05 +0530

    contact form css tweaks

--- client-side/src/component/ContactUs.jsx
+++ client-side/src/component/ContactUs.jsx
@@ -107,7 +107,7 @@ function ContactUs() {
         >
           <FaChevronLeft className="text-white text-3xl mb-6" />
         </div>
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white mt-[30px] lg:mt-[20px]">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white mt-[30px] lg:mt-[20px]">
           Contact Us
         </h2>
         <div className="flex flex-wrap w-full text-slate-400 mt-[35px] md:mt-[50px] mb-[50px] px-5 justify-evenly items-center gap-5 md:gap-0">
@@ -128,7 +128,7 @@ function ContactUs() {
         </div>
         {/* line */}
         <div className="h-[2px] w-full bg-slate-500 mb-[50px]"></div>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-400 sm:text-xl">
           If any difficulties in contacting us through phone or whatsapp ,
           message us we will get back to you asap.
         </p>
@@ -136,14 +136,14 @@ function ContactUs() {
           <div>
             <label
               htmlFor="order-name"
-              className="block mb-2 text-sm font-medium max-w-max text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm font-medium max-w-max text-gray-300"
             >
               Your Name
             </label>
             <input
               type="text"
               id="order-name"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="shadow-sm border text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
               placeholder="for eg: Rishabh Malik"
               required
             />
@@ -151,14 +151,14 @@ function ContactUs() {
           <div>
             <label
               htmlFor="subject"
-              className="block mb-2 text-sm max-w-max font-medium text-gray-900 dark:text-gray-300"
+              className="block mb-2 text-sm max-w-max font-medium text-gray-300"
             >
               Subject
             </label>
             <input
               type="text"
               id="subject"
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="block p-3 w-full text-sm rounded-lg border shadow-sm bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
               placeholder="Let us know how we can help you"
               required
             />
@@ -166,7 +166,7 @@ function ContactUs() {
           <div>
             <label
               htmlFor="contact_number"
-              className="mb-2 block text-sm max-w-max font-medium text-gray-900 dark:text-white"
+              className="mb-2 block text-sm max-w-max font-medium text-white"
             >
               {" "}
               Phone Number{" "}
@@ -253,14 +253,14 @@ function ContactUs() {
           <div className="sm:col-span-2">
             <label
               htmlFor="message"
-              className="block mb-2 text-sm max-w-max font-medium text-gray-900 dark:text-gray-400"
+              className="block mb-2 text-sm max-w-max font-medium text-gray-400"
             >
               Your message
             </label>
             <textarea
               id="message"
               rows="6"
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="block p-2.5 w-full text-sm rounded-lg shadow-sm border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
               placeholder="Briefly explain us your problem.."
             ></textarea>
           </div>
